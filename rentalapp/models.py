75f7fd606a785e9d8b3c61fd9c443be1dd377eca
Adnan Ahmed Khan <iamkhanadnan@gmail.com>
2021-01-10 20:18:30 +0600

    Adds created_at/updates_at to model, migrates initial migrations

--- rentalapp/models.py
+++ rentalapp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 
-class Profile(models.Model):
+class RentalModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Profile(RentalModel):
     mobile = models.CharField(max_length=11)
     name = models.CharField(max_length=100)
     photo_url = models.CharField(max_length=200)
@@ -9,20 +14,18 @@ class Profile(models.Model):
     password = models.CharField(max_length=200)
     reference_id = models.IntegerField()
     logged_in_at = models.DateField()
-    created_at = models.DateField()
-    updated_at = models.DateField()
     otp = models.IntegerField()
     otp_expiry = models.DateField()
     status = models.IntegerField()
 
 
-class House(models.Model):
+class House(RentalModel):
     name = models.CharField(max_length=200)
     owner_id = models.IntegerField()
     address_id = models.IntegerField()
 
 
-class Address(models.Model):
+class Address(RentalModel):
     house_id = models.IntegerField()
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
@@ -30,28 +33,28 @@ class Address(models.Model):
     long = models.FloatField()
 
 
-class Properties(models.Model):
+class Properties(RentalModel):
     name = models.CharField(max_length=200)
     index = models.IntegerField()
 
 
-class HouseProperties(models.Model):
+class HouseProperties(RentalModel):
     house_id = models.IntegerField()
     property_id = models.IntegerField()
     value = models.CharField(max_length=200)
 
 
-class Review(models.Model):
+class Review(RentalModel):
     user_id = models.IntegerField()
     comment = models.CharField(max_length=200)
     rating = models.CharField(max_length=200)
 
 
-class Wishlist(models.Model):
+class Wishlist(RentalModel):
     user_id = models.IntegerField()
     house_id = models.IntegerField()
 
 
-class Auth(models.Model):
+class Auth(RentalModel):
     user_id = models.IntegerField()
     device_id = models.IntegerField()
